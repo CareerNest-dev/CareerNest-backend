9eb15932cddc52dad457b27520d870e4be66b9ba
CareerNest <udarachamod03@gmail.com>
2025-08-03 23:25:34 +0530

    Update s3_bucket_config.js

--- config/aws/s3_bucket_config.js
+++ config/aws/s3_bucket_config.js
@@ -6,10 +6,10 @@ dotenv.config();
 // S3 client configuration
 const s3Client = new S3Client({
   region: process.env.AWS_REGION_DYNAMODB || "ap-south-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+  // credentials: {
+  //   // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // },
 });
 
 export default s3Client;
